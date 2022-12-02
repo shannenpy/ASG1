80a530e870ffc1d73a03c0b1f50d052402f47afe
shannenpy <s10244203@connect.np.edu.sg>
2022-12-03 01:12:41 +0800

    homepage content

--- homepage.js
+++ homepage.js
@@ -11,13 +11,13 @@ const navContent =
 `<li class="nav-content"><a href = "homepage.html">The Digital Zone</a></li>
 <li class="nav-content"><a href = "account.html">Login/ Signup</a></li>
 <li class="nav-content"><a href = "product.html">Products & Services</a></li>
-<li class="nav-content"><a href = "contact.html">Contact Us</a></li>
 <li class="nav-content"><a href = "review.html">Reviews</a></li>
-<li class="nav-content"><a href = "pricing.html">Pricings</a></li>`
+<li class="nav-content"><a href = "pricing.html">Pricings</a></li>
+<li class="nav-content"><a href = "contact.html">Contact Us</a></li>`
 navList.innerHTML = navContent
 
 // homepage
-let homePage = document.createElement("section")
+let homePage = document.createElement("div")
 homePage.classList.add("homepage")
 document.body.appendChild(homePage)
 var homepage = document.querySelector(".homepage")
@@ -25,23 +25,41 @@ var homepage = document.querySelector(".homepage")
 let productServices = document.createElement('h2')
 productServices.classList.add("products-services")
 homepage.append(productServices)
-var productServicesContent = `Products & Services`
-productServices.innerHTML = productServicesContent
+var productServicesHeading = `Products`
+productServices.innerHTML = productServicesHeading
 
-let contact = document.createElement("h2")
+let pricing = document.createElement("section")
+pricing.classList.add("pricing")
+homepage.append(pricing)
+var pricingHeading = `<h2>Pricings</h2>`
+pricing.innerHTML = pricingHeading
+var pricingContent =
+`<p class="pricingContent">Most Popular!!</p>
+<p class="pricingContent">Full access with <strong>yearly</strong> subscription at only <strong>$149</strong></p>`
+pricing.innerHTML = pricingContent
+
+let review = document.createElement('section')
+review.classList.add("review")
+homepage.append(review)
+var reviewHeading = `<h2>Reviews</h2>`
+review.innerHTML = reviewHeading
+let reviewContentList = document.createElement("ul")
+review.append(reviewContentList)
+var reviewContent = 
+`<li>Isobella Mckee: This website is easy to use and has great templates. Highly recommend!</li>
+<li>Olive Wall: Always come here for their templates when I need to start a new project.</li>
+<li>Nevaeh Coffey: Many themes to choose from and helps me start my project easily. Strongly recommend to get!!</li>`
+reviewContentList.innerHTML = reviewContent
+
+let contact = document.createElement("section")
 contact.classList.add("contact")
 homepage.append(contact)
-var contactContent = `Contact Us`
-contact.innerHTML = contactContent
-
-let pricings = document.createElement("h2")
-pricings.classList.add("pricings")
-homepage.append(pricings)
-var pricingsContact = `Pricings`
-pricings.innerHTML = pricingsContact
-
-let reviews = document.createElement('h2')
-reviews.classList.add("reviews")
-homepage.append(reviews)
-var reviewsContent = `Reviews`
-reviews.innerHTML = reviewsContent
+var contactHeading = `<h2>Contact Us</h2>`
+contact.innerHTML = contactHeading
+let contactContentList = document.createElement("ul")
+contact.append(contactContentList)
+var contactContent = 
+`<li>Tel: 67836755</li>
+<li>Email: queries@tdz.com</li>
+<li>Instagram: TheDigitalZone</li>`
+contactContentList.innerHTML = contactContent
--- product.js
+++ product.js
@@ -9,7 +9,7 @@ const navContent =
 `<li class="nav-content"><a href = "homepage.html">The Digital Zone</a></li>
 <li class="nav-content"><a href = "account.html">Login/ Signup</a></li>
 <li class="nav-content"><a href = "product.html">Products & Services</a></li>
-<li class="nav-content"><a href = "contact.html">Contact Us</a></li>
 <li class="nav-content"><a href = "review.html">Reviews</a></li>
-<li class="nav-content"><a href = "pricing.html">Pricings</a></li>`
+<li class="nav-content"><a href = "pricing.html">Pricings</a></li>
+<li class="nav-content"><a href = "contact.html">Contact Us</a></li>`
 navList.innerHTML = navContent
